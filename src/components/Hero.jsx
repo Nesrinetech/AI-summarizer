3e91fdf54099dbf3ebecad0a7aed0ccd8c7394b9
Nesrine <kabachinesrine34@gmail.com>
2024-03-04 13:51:06 +0100

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,15 +8,7 @@ const Hero = () => {
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
         <img src={logo} alt='logo' className='w-28 object-contain' />
-        <button
-          type='button'
-          onClick={() =>
-            window.open("https://github.com/Nesrinetech", "_blank")
-          }
-          className='black_btn'
-        >
-          GitHub
-        </button>
+        
       </nav>
 
       <h1 className='head_text'>
