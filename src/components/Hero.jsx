57bf4d17c5c868abd3dd361252ffb78b12bfbb03
Nesrine <kabachinesrine34@gmail.com>
2024-03-29 14:40:31 +0100

    good

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,15 +6,15 @@ import "../App.css";   // Make sure to import your CSS file
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
-      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='logo' className='w-28 object-contain' />
+      <nav className='flex justify-between items-center w-full mb-10 pt-6 mr-12'>
+        <img src={logo} alt='logo' className='w-28 object-contain ml-0 mr-3' />
         
       </nav>
 
       <h1 className='head_text'>
-        <span className='purple_gradient'>Nesrine's AI summarizer</span><br></br>
-        <br className='max-md:hidden' />
-        <span className='purple_gradient'>Your tool for quick summaries.</span>
+        <span className='purple_gradient'>Sumlify <br></br>Your best tool </span><br></br>
+        
+        <span className='purple_gradient'>for quick summaries.</span>
       </h1>
       <h2 className='desc'>
         Simplify your reading! Just paste an article or webpage link, and let the app create concise summaries for quick understanding.
